a869dc70d9d88c4eb6493945884114a5a387e493
Tapfuma <enosmhundwa@gmail.com>
2019-09-01 20:59:38 +0200

    update css and html

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ session_start();
 
 	<title>Todo List</title>
 
-	<link rel="stylesheet" type="text/css" href="assets/css/todos.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/todo.css">
 	<link href='https://fonts.googleapis.com/css?family=Roboto:400,700,500' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.css">
 
